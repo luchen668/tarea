9bf902fae77752859d5f72fa58bbccc4879b47d8
luchen669 <Arielgusmam605@gmil.com>
2022-11-17 13:01:28 -0400

    first commit

---
+++ Periodo.js
@@ -0,0 +1,14 @@
+const aSmile = true;
+const bSmile = false;
+const alerta = "Verdader;Tamo en problem";
+const alerta1 = "falso;Tamo en problem";
+
+if(aSmile == false && bSmile == false || aSmile == true && bSmile == true)
+{
+    console.log(alerta)
+}
+
+else
+{
+    console.log(alerta1)
+}
